03e147c7caec086c4058c8b52c5466e00791bd7c
99003659 <78849786+99003659@users.noreply.github.com>
2021-03-02 14:54:31 +0530

    Update mystring.c

--- Src/mystring.c
+++ Src/mystring.c
@@ -1,27 +1,51 @@
-#include "bit_set_reset.h"
+#include "mystring.h"
 #include <stdio.h>
 
-int set(int n, int k)
+char* mystrcpy(char* pdest,const char *psrc)  //const char pstr[]
 {
-    return (n | (1 << (k - 1)));
+  int k=0;
+  while(psrc[k]!='\0')  //while(pdest[k++]=psrc[k])
+  {
+    pdest[k]=psrc[k];
+    k++;
+  }
+  pdest[k]='\0';
+  //return k;
 }
 
-// Function to clear the kth bit of n
-int clear(int n, int k)
+int mystrlen(const char *pstr)  //const char pstr[]
+	{
+	  int k=0;
+	  while(pstr[k]!='\0')  //while(pstr[k++])
+	    k++;
+	  return k;
+	}
+
+char* mystrcat(char* pdest,const char *psrc)  //const char pstr[]
 {
-    return (n & (~(1 << (k - 1))));
+  int i=0,j=0;
+  while(pdest[i]!='\0')
+    i++;
+  while(psrc[j]!='\0')  //while(pdest[i++]=psrc[j++])
+  {
+    pdest[i]=psrc[j];
+    i++;
+    j++;
+  }
+  pdest[i]='\0';
+  return pdest;
 }
 
-// Function to toggle the kth bit of n
-int flip(int n, int k)
-{
-    return (n ^ (1 << (k - 1)));
-}
 
-int query(int n, int k)
+int mystrcmp(const char* ps1,const char* ps2)
 {
-    if ((n >> (k - 1)) & 1)
-        return SET;
-    else
-        return NOTSET;
+  int i=0;
+  while(ps1[i]==ps2[i]&&ps1[i]!='\0'&&ps2[i]!='\0')
+    i++;
+  if(ps1[i]==ps2[i])
+    return 0;
+  else if (ps1[i] < ps2[i])
+    return -1;
+  else //ps1[i] > ps2[i]
+    return +1;
 }
